a0d606bf2dacb22d0d993442158d6a9b43ccc1d9
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-04-25 19:40:08 +0300

    fuck typescript

---
+++ src/app/services/data/place.js
@@ -0,0 +1,14 @@
+
+export const places =
+    [{
+        "_id": "p101",
+        "title": "Tel Aviv",
+        "description": "tel aviv ya habibi",
+        "imgUrl": "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1",
+        "createdAt": "1619367824710",
+        "comments": [],
+        "location": {
+            "lan": 424,
+            "lat": 424
+        }
+    }]
